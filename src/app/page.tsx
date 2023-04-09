e30a87b2907d00169c615c2867567a17c151554d
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-09 23:11:49 +0530

    implemented authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { db } from "@/lib/db";
+import Button from "@/components/ui/Button";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default async function Home() {
 	await db.set("hello", "hello");
 
-	return <div>Hii</div>;
+	return <Button>Hii</Button>;
 }
