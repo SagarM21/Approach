f47df4f0f4bf79d0e9c76e0cc2b2cf1f5cd7d65b
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-09 22:47:05 +0530

    integrated upstash redis

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
+import { db } from "@/lib/db";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home() {
-	return (
-		<main className='flex min-h-screen flex-col items-center justify-between p-24'>
-			<div>Hii</div>
-		</main>
-	);
+export default async function Home() {
+	await db.set("hello", "hello");
+
+	return <div>Hii</div>;
 }
