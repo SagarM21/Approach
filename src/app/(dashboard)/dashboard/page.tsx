8f4202e1da91a07201e923a685172781b41cae76
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-12 22:59:29 +0530

    add friend screen

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,10 +1,13 @@
-import Button from "@/components/ui/Button";
+import AddFriendButton from "@/components/AddFriendButton";
 import { FC } from "react";
 
-interface pageProps {}
-
-const page: FC<pageProps> = ({}) => {
-	return <Button>Hello</Button>;
+const page: FC = () => {
+	return (
+		<main className='pt-8'>
+			<h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
+			<AddFriendButton />
+		</main>
+	);
 };
 
 export default page;
