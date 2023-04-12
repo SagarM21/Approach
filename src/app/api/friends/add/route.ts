0febef7d181e4c29ae766ed560079bb97b7ac0a7
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-12 23:29:32 +0530

    Adding users as frnds

---
+++ src/app/api/friends/add/route.ts
@@ -0,0 +1,33 @@
+import { authOptions } from "@/lib/auth";
+import { addFriendValidator } from "@/lib/validations/add-friend";
+import { getServerSession } from "next-auth";
+
+export async function POST(req: Request) {
+	try {
+		const body = await req.json();
+		const { email: emailToAdd } = addFriendValidator.parse(body.email);
+		const RESTResponse = await fetch(
+			`${process.env.UPSTASH_REDIS_REST_URL}/get/user:email${emailToAdd}`,
+			{
+				headers: {
+					Authorization: `Bearer ${process.env.UPSTASH_REDIS_REST_TOKEN}`,
+				},
+				cache: "no-store",
+			}
+		);
+
+		const data = (await RESTResponse.json()) as { result: string | null };
+		const idTodd = data.result;
+		if (!idTodd)
+			return new Response("This person does not exist.", { status: 400 });
+		const session = await getServerSession(authOptions);
+		if (!session) {
+			return new Response("Unauthorized", { status: 401 });
+		}
+		if (idTodd === session.user.id) {
+			return new Response("You cannot add yourself as a friend", {
+				status: 400,
+			});
+		}
+	} catch (error) {}
+}
