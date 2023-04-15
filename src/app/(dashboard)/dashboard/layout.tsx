c5d5ea200b9fb5525e303f40d449d869edf7ce45
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-15 11:57:24 +0530

    fix: module import error

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,3 +1,4 @@
+import { Icon, Icons } from "@/components/Icons";
 import SignOutButton from "@/components/SignOutButton";
 import { authOptions } from "@/lib/auth";
 import { getServerSession } from "next-auth";
@@ -9,9 +10,8 @@ import FriendRequestSidebarOptions from "@/components/FriendRequestSidebarOption
 import { fetchRedis } from "@/helpers/redis";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
 import SidebarChatList from "@/components/SidebarChatList";
-import { SidebarOption } from "@/types/typings";
 import MobileChatLayout from "@/components/MobileChatLayout";
-import { Icons } from "@/components/Icons";
+import { SidebarOption } from "@/types/typings";
 
 interface LayoutProps {
 	children: ReactNode;
@@ -56,6 +56,7 @@ const Layout = async ({ children }: LayoutProps) => {
 					unseenRequestCount={unseenRequestCount}
 				/>
 			</div>
+
 			<div className='hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
 				<Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
 					<Icons.Logo className='h-8 w-auto text-indigo-600' />
