880bf1cd2a5c5f4e53a3fc3453ec4af697d2465a
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-15 11:07:53 +0530

    bugFix:  rtc when frnd request send

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -39,6 +39,13 @@ export async function POST(req: Request) {
 			return new Response("No friend request", { status: 400 });
 		}
 
+		// notify added user
+		pusherServer.trigger(
+			toPusherKey(`user:${idToAdd}:friends`),
+			"new_friend",
+			{}
+		);
+
 		await db.sadd(`user:${session.user.id}:friends`, idToAdd);
 		await db.sadd(`user:${idToAdd}:friends`, session.user.id);
 		await db.srem(`user:${session.user.id}:incoming_friend_requests`, idToAdd);
