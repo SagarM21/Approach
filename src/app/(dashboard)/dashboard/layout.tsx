0476d9e239072bd6cfa3a111369ea736ec6ed4ff
Sagar Maheshwari <sgr.mhshwr22@gmail.com>
2023-04-15 11:34:47 +0530

    add: mobile chat ui ready @headlessui

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -11,6 +11,7 @@ import { fetchRedis } from "@/helpers/redis";
 import { getFriendsByUserId } from "@/helpers/get-friends-by-user-id";
 import SidebarChatList from "@/components/SidebarChatList";
 import { SidebarOption } from "@/types/typings";
+import MobileChatLayout from "@/components/MobileChatLayout";
 
 interface LayoutProps {
 	children: ReactNode;
@@ -47,6 +48,14 @@ const Layout = async ({ children }: LayoutProps) => {
 
 	return (
 		<div className='w-full flex h-screen'>
+			<div className='md:hidden'>
+				<MobileChatLayout
+					friends={friends}
+					session={session}
+					sidebarOptions={sidebarOptions}
+					unseenRequestCount={unseenRequestCount}
+				/>
+			</div>
 			<div className='hidden md:flex h-full w-full max-w-xs grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6'>
 				<Link href='/dashboard' className='flex h-16 shrink-0 items-center'>
 					<Icons.Logo className='h-8 w-auto text-indigo-600' />
